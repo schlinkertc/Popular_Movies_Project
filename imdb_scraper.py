54192da143ab1f8f67f1aa1a6d578336cdf9b224
Charlie Schlinkert <schlinkertc@gmail.com>
2019-11-01 09:45:48 -0400

    functions added

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -5,97 +5,74 @@ from bs4 import BeautifulSoup
 import json
 
 
-## takes in a list of ids and returns rating data in a list of dictionaries
+## takes in a list of ids and returns data in a list of dictionaries
 
-def find_rating(imdb_ids):
-    ratings = []
+def get_all(imdb_ids):    
+    imdb_table = []
     for imdb_id in imdb_ids:
-        try:
-            url = 'https://www.imdb.com/title/{}/'.format(imdb_id)
-
-            r = requests.get(url)
-            page = BeautifulSoup(r.content)
-            post = page.find_all(attrs={'class':'ratingValue'})
-
-            rating_value = float(post[0].find(itemprop="ratingValue").contents[0])
-            num_user_ratings = float(list(post[0].find('strong').attrs.values())[0].split()[3].replace(',',''))
-
-            rating = {}
-            rating['imdb_id'] = imdb_id
-            rating['value'] = rating_value
-            rating['num_user_ratings'] = num_user_ratings
-
-            ratings.append(rating)
-        except: 
-            None
-    return ratings
-
-## takes in a list of ids and returns [{id:id, cast:10 cast members}]
-
-def get_credits(imdb_ids):
-    credits = []
-    for imdb_id in imdb_ids:
-        url = 'https://www.imdb.com/title/{}/fullcredits/'.format(imdb_id)
-
+        movie_dict = {}
+        movie_dict['imdb_id'] = imdb_id
+        #connect to the main page for the movie 
+        
+        cast_url = 'https://www.imdb.com/title/{}/fullcredits/'.format(imdb_id)
+        url = 'https://www.imdb.com/title/{}/'.format(imdb_id)
         r = requests.get(url)
         page = BeautifulSoup(r.content)
         
+        
+        #get the cast members
         odd = page.find_all(attrs={'class':'odd'})
         even = page.find_all(attrs={'class':'even'})
         names = []
-        for item in odd:
-            names.append(item.find_all('a')[1].text.strip())
-        for item in even:
-            names.append(item.find_all('a')[1].text.strip())
-            
-        movie_credits = {}
-        movie_credits['imdb_id'] = imdb_id
-        movie_credits['cast'] = names[:10]
         
-        credits.append(movie_credits)
-    return credits
-
-def get_keywords(imdb_ids):
-    keywords = []
-    for imdb_id in imdb_ids:
-        url = 'https://www.imdb.com/title/{}/'.format(imdb_id)
-        
-        r = requests.get(url)
-        page = BeautifulSoup(r.content)
-        post = page.find_all(attrs={'class':'itemprop'})
-        
-        words = [x.getText() for x in post]
-        
-        keyword_dict = {}
-        keyword_dict['id'] = imdb_id
-        keyword_dict['keywords'] = words
-        
-        keywords.append(keyword_dict)
-        
-    return keywords
-
-def get_box_office(imdb_ids):
-    box_office = []
-    for imdb_id in imdb_ids:
         try:
-            url = 'https://www.imdb.com/title/{}/'.format(imdb_id)
-
-            r = requests.get(url)
-            page = BeautifulSoup(r.content)
-            post = page.find_all(attrs={'class':['txt-block']})
-
-            budget = int([x.getText().split()[0] for x in post][9:12:2][0].split('$')[1].replace(',',''))
-            gross = int([x.getText().split() for x in post][9:12:2][1][-1].strip('$').replace(',',''))
-
-            box_office_dict={}
-            box_office_dict['id'] = imdb_id
-            box_office_dict['budget'] = budget
-            box_office_dict['gross'] = gross 
-
-            box_office.append(box_office_dict)
+            for item in odd:
+                names.append(item.find_all('a')[1].text.strip())
+            for item in even:
+                names.append(item.find_all('a')[1].text.strip())
+            
+            movie_dict['cast'] = ', '.join(names[:10])
+        
         except:
-            box_office_dict={}
-            box_office_dict['id'] = None
-            box_office_dict['budget'] = None
-            box_office_dict['gross'] = None
-    return box_office
+            movie_dict['cast'] = None
+        
+        #get the keywords
+        
+        words_post = page.find_all(attrs={'class':'itemprop'})
+
+        words = [x.getText() for x in words_post]
+        movie_dict['keywords'] = ', '.join(words)
+
+        #get box office info
+        
+        try:
+            box_office_post = page.find_all(attrs={'class':['txt-block']})
+
+            budget = int([x.getText().split()[0] for x in box_office_post][9:12:2][0].split('$')[1].replace(',',''))
+            gross = int([x.getText().split() for x in box_office_post][9:12:2][1][-1].strip('$').replace(',',''))
+
+            movie_dict['budget'] = budget
+            movie_dict['gross'] = gross 
+
+        except:
+            movie_dict['budget'] = None
+            movie_dict['gross'] = None
+        
+        #get rating info 
+        
+        try:
+
+            ratings_post = page.find_all(attrs={'class':'ratingValue'})
+
+            rating_value = float(ratings_post[0].find(itemprop="ratingValue").contents[0])
+            num_user_ratings = float(list(ratings_post[0].find('strong').attrs.values())[0].split()[3].replace(',',''))
+
+            movie_dict['rating_value'] = rating_value
+            movie_dict['rating_count'] = num_user_ratings
+
+        except: 
+            movie_dict['rating_value'] = None
+            movie_dict['rating_count'] = None
+
+        imdb_table.append(movie_dict)
+    return imdb_table
--- movieDB_functions.py
+++ movieDB_functions.py
@@ -5,7 +5,7 @@ import vulture_scraper as vulture
 api_key = config.API_key
 import time
 import datetime 
-
+import query_helper
 
 ## search a list of terms on MovieDB and return the results in a list of dictionaries 
 def search_movies_timed(api_key, list_of_movies):
@@ -41,10 +41,14 @@ def get_movies_timed(api_key,ids):
 
 
 ## takes in a list of strings, and performs search and get functions simultaneously 
+query_helper.connect()
 def search_and_get(api_key,list_of_movies):
     search = search_movies_timed(api_key,list_of_movies)
     
-    ids = [d['id'] for d in search]
+    exists = query_helper.query("""select imdb_id from Movies.tmdb_;""")
+    exists = [x[0] for x in exists]
+    
+    ids = [d['id'] for d in search if d['id'] not in exists]
     
     return get_movies_timed(api_key,ids)
 
@@ -101,3 +105,93 @@ def get_movies(api_key,ids):
         time.sleep(.5)
         data.append(requests.get(url).json())
     return data
+
+def parse_function(search_results):
+    results = []
+    for d in search_results:
+        if (d['imdb_id'] != '' and d['imdb_id'] != None and d['status'] == 'Released'):
+            parsed_results = {}
+
+            parsed_results['id'] = d.get('id')
+            parsed_results['original_language'] = d['original_language']
+            parsed_results['overview'] = d.get('overview')
+            parsed_results['title'] = d.get('title')
+            parsed_results['popularity'] = d.get('popularity')
+
+            if d['belongs_to_collection'] == None:
+                parsed_results['collection'] = None
+            else: 
+                parsed_results['collection'] = d['belongs_to_collection']['name']
+                #make list into str
+                temp = ''
+                for i in parsed_results['collection']:
+                    temp += i + ' '
+                parsed_results['collection'] = temp
+
+            parsed_results['budget'] = d.get('budget')
+            parsed_results['revenue'] = d.get('revenue')
+
+            if len(d['genres']) == 0:
+                parsed_results['genres'] = None
+            else:
+                parsed_results['genres'] = [x['name'] for x in d['genres']]
+                                #make list into str
+                temp = ''
+                for i in parsed_results['genres']:
+                    temp += i + ' '
+                parsed_results['genres'] = temp
+
+
+            if len(d['production_companies']) == 0:
+                parsed_results['production_company'] = None
+            else:
+                parsed_results['production_company'] = [x['name'] for x in d['production_companies']]
+                                                #make list into str
+                temp = ''
+                for i in parsed_results['production_company']:
+                    temp += i + ' '
+                parsed_results['production_company'] = temp
+
+            if len(d['production_countries']) == 0:
+                parsed_results['production_countries'] = None
+            else:
+                parsed_results['production_countries'] = [x['name'] for x in d['production_countries']]
+                                                #make list into str
+                temp = ''
+                for i in parsed_results['production_countries']:
+                    temp += i + ' '
+                parsed_results['production_countries'] = temp
+
+            if d['release_date'] == '':
+                parsed_results['release_date'] = None 
+            else:
+                parsed_results['release_date'] = datetime.datetime.strptime(d['release_date'],"%Y-%m-%d").date()
+
+            if d['runtime'] == None:
+                parsed_results['run_time'] = None
+            else:
+                parsed_results['run_time'] = d['runtime']
+           
+            if len(d['spoken_languages']) == 0:
+                parsed_results['spoken_lang'] = None 
+            else:
+                parsed_results['spoken_lang'] = [x['name'] for x in d['spoken_languages']]
+                                                #make list into str
+                temp = ''
+                for i in parsed_results['spoken_lang']:
+                    temp += i + ' '
+                parsed_results['spoken_lang'] = temp
+            
+            if d['tagline'] == '':
+                parsed_results['tagline'] = None
+            else: 
+                parsed_results['tagline'] = d['tagline']
+            
+            parsed_results['vote_avg'] = d['vote_average']
+            parsed_results['vote_count'] = d['vote_count']
+            parsed_results['imdb_id'] = d['imdb_id']
+            parsed_results['vote_count'] = d['vote_count']
+            
+            
+            results.append(parsed_results)
+    return results
